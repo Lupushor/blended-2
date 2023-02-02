5d3c81c48eab437ecea4cc8bbe4687ea9926ea8e
Lupushor <103314959+Lupushor@users.noreply.github.com>
2023-02-02 11:05:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -245,8 +245,10 @@ console.log( getUsersWithAge( users, 30, 40 ) );
 // 7.8
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
-// console.log(getUsersWithFriend(users, 'Briana Decker')) // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+const getUsersWithFriend = (users, friendName) => {users.filter (user => user.friends.includes(friendName))};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')) // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
 // 7.9
 // Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
